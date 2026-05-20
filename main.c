8ef67c5a289c5183e6f955cf0ebf9038e40c6e69
Alessandro Reginato <alessandror37@gmail.com>
2026-05-20 19:02:45 -0400

    Creación del proyecto

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Entrada{
+    int idEntrada;
+    int tipo; /*0-> Entrada general, 1-> Pase infantil, 2-> Entrada familiar, 3 -> Pase VIP*/
+    int valor;
+    char *estado;
+};
+
+
+/*Lista simplemente enlazada con nodo fantasma*/
+struct NodoEntrada{
+    struct Entrada *datos;
+    struct NodoEntrada *sig;
+};
+
+struct	Visitante{
+    int idVisitante; /*Al ser manejados con árboles, vamos a tener que investigar sobre como manejar los ids para
+    obtener un arbol binario*/
+    char *rut;
+    char *nombre;
+    struct NodoEntrada *headEntradas; /*head lista entradas de cada visitante*/
+    struct Zona *zonaActual;
+};
+
+/*ABB*/
+struct NodoVisitante{
+    struct Visitante *datos;
+    struct NodoVisitante *izq,*der;
+};
+
+/*Lista simple circular con nodo fantasma*/
+struct NodoFila{
+    struct Visitante *datos;
+    struct NodoFila *sig;
+};
+
+struct Atraccion{
+    int estado; //0 -> Operativa, 1 -> En mantenimiento, 2 ->  Fuera de servicio, 3 -> Cerrdada por horario
+    int capacidad;
+    int duracion; /*duración en minutos*/
+    float alturaMinima;/*altura en metros*/
+    int edadMinima;
+    struct Nodofila *headFila; /*head a la lista de la fila de la atracción*/
+    int visitantesTotales;
+};
+
+/*Lista doblemente enlazada con nodo fantasma*/
+struct NodoAtraccion{
+    struct Atraccion *datos;
+    struct NodoAtraccion *ant,*sig;
+};
+
+
+struct Zona{
+    char *nombre;
+    int codigo;
+    char *tematica;
+    char *horaInicio;
+    char *horaCierre;
+    int capacidad;
+    int personalEncargado;
+    struct NodoAtraccion *headAtracciones; /*head a la lista de atracciones*/
+};
+
+
+struct Parque {
+    int recaudacionTotal;
+    int totalVisitantes;
+    struct Zona **zonas;
+    int pLibreZonas;
+    struct NodoVisitante *headVisitantes;
+};
+
+int main() {
+    printf("Hola profe ponganos un 7");
+    return 0;
+}
