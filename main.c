08d54bc0a6bf58b5b524593c465f28d5e7feba6d
Alessandro Reginato <alessandror37@gmail.com>
2026-05-25 19:32:49 -0400

    Creado un esqueleto del main del programa y de la seccion visitantes

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 
 #define CAPACIDAD_MAX 100
+#define MAX_ID_VISITANTES 100000 /*sintaxis para la generacion de id: id = rand() % (MAX_ID_VISITANTES + 1)*/
+
 
 struct Entrada{
     int idEntrada;
@@ -21,7 +25,7 @@ struct NodoEntrada{
 struct	Visitante{
     int idVisitante; /*Al ser manejados con árboles, vamos a tener que investigar sobre como manejar los ids para
     obtener un arbol binario*/
-    int boolEstaEnParque;
+    int boolEstaEnParque; /*0-> No esta en parque, 1 -> Si esta en parque*/
     char *rut;
     char *nombre;
     struct NodoEntrada *headEntradas; /*head lista entradas de cada visitante*/
@@ -43,7 +47,7 @@ struct NodoFila{
 struct Atraccion{
     int estado; /*0 -> Operativa, 1 -> En mantenimiento, 2 ->  Fuera de servicio, 3 -> Cerrdada por horario*/
     int capacidad;
-    struct Visitante visitantesEnAtraccion[CAPACIDAD_MAX]; /*La capacidad real de la atracción es la misma que se encuenta
+    struct Visitante visitantesEnAtraccion[CAPACIDAD_MAX]; /*La capacidad real de la atracción es la misma que se encuentra
     dentro del struct y es la que se va a tomar en cuenta para la lógica de las funciones*/
     int duracion; /*duración en minutos*/
     float alturaMinima;/*altura en metros*/
@@ -79,7 +83,46 @@ struct Parque {
     struct NodoVisitante *headVisitantes; /*head a la raiz de arbol visitantes*/
 };
 
-int main() {
-    printf("Hola profe ponganos un 7");
+int agregarVisitante() {
+
+}
+
+int contarVisitantesEnParque(struct NodoVisitante *raiz) {
+    int cont = 0;
+
+    if (raiz == NULL) {
+        return 0;
+    }
+    cont += contarVisitantesEnParque(raiz->izq);
+    cont += contarVisitantesEnParque(raiz->der);
+
+    return cont + raiz->datos->boolEstaEnParque;
+}
+void mostrarMenuVisitantes(void) {
+
+}
+
+
+
+int main(void) {
+    int opcionMenu;
+
+    printf("Bienvenido al menu de IBCLandia\n");
+    printf("1.- Menu de visitantes\n");
+    printf("2.- Menu de entradas");
+    printf("3.- Menu de atracciones");
+    printf("4.- Menu de zonas");
+    printf("5.- Menu de datos");
+
+    printf("Ingrese operacion deseada:");
+    scanf("%d",&opcionMenu);
+    switch (opcionMenu) {
+        case 1:
+            mostrarMenuVisitantes();
+        default:
+            printf("Ingrese una opcion valida.");
+
+
+    }
     return 0;
 }
