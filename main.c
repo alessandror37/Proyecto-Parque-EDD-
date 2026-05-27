4716d889d14c93afd402ac614c56460c44f7fcb3
SilentlyJon <j.gutierrez1764@gmail.com>
2026-05-26 22:31:41 -0400

    ola agregué 4 funciones ekisde

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+char fechaActual[10];
 
 struct Entrada{
     int idEntrada;
     int tipo; /*0-> Entrada general, 1-> Pase infantil, 2-> Entrada familiar, 3 -> Pase VIP*/
     int valor;
-    char *estado;
+    int estado; /* */
+    char *fechaUsada;
 };
-
 
 /*Lista simplemente enlazada con nodo fantasma*/
 struct NodoEntrada{
@@ -16,7 +19,7 @@ struct NodoEntrada{
     struct NodoEntrada *sig;
 };
 
-struct	Visitante{
+struct Visitante{
     int idVisitante; /*Al ser manejados con árboles, vamos a tener que investigar sobre como manejar los ids para
     obtener un arbol binario*/
     char *rut;
@@ -38,12 +41,13 @@ struct NodoFila{
 };
 
 struct Atraccion{
-    int estado; /*0 -> Operativa, 1 -> En mantenimiento, 2 ->  Fuera de servicio, 3 -> Cerrdada por horario */
+    char *nombre;
+    int estado; /*0 -> Operativa, 1 -> En mantenimiento, 2 -> Fuera de servicio, 3 -> Cerrada por horario */
     int capacidad;
     int duracion; /*duración en minutos*/
     float alturaMinima;/*altura en metros*/
     int edadMinima;
-    struct Nodofila *headFila; /*head a la lista de la fila de la atracción*/
+    struct NodoFila *headFila; /*head a la lista de la fila de la atracción*/
     int visitantesTotales;
 };
 
@@ -52,7 +56,6 @@ struct NodoAtraccion{
     struct Atraccion *datos;
     struct NodoAtraccion *ant,*sig;
 };
-
 
 struct Zona{
     char *nombre;
@@ -65,16 +68,116 @@ struct Zona{
     struct NodoAtraccion *headAtracciones; /*head a la lista de atracciones*/
 };
 
-
-struct Parque {
+struct Parque{
     int recaudacionTotal;
     int totalVisitantes;
     struct Zona **zonas;
-    int pLibreZonas;
+    int *pLibreZonas;
     struct NodoVisitante *headVisitantes;
 };
 
+char *pasarAMinus(char *cadena){
+    int i;
+    int largo = strlen(cadena) + 1;
+    char *NuevaCadena = (char *)malloc(sizeof(char) * largo);
+
+    for(i = 0; cadena[i] != '\0'; i++){
+        NuevaCadena[i] = tolower((unsigned char)cadena[i]);
+    }
+    return NuevaCadena;
+}
+
+struct NodoAtraccion *crearAtraccion(){
+    /*Asignacion de memoria a los nuevos struct e inicialización de variables*/
+    struct Atraccion *atraccionNueva = malloc(sizeof(struct Atraccion));
+    struct NodoAtraccion *nuevoNodo = malloc(sizeof(struct NodoAtraccion));
+    char bufferNombre[100];
+
+    /* fase de llenado por parte del usuario */
+    printf("Ingrese el nombre de la atracción\n");
+    fgets(bufferNombre, 100, stdin);
+
+    bufferNombre[strcspn(bufferNombre, "\n")] = '\0';
+    atraccionNueva -> nombre = (char *)malloc(sizeof(char) * (strlen(bufferNombre) + 1) );
+    strcpy(atraccionNueva -> nombre, bufferNombre);
+
+    printf("Ingrese la capacidad de la atraccion\n");
+    scanf("%d", &(atraccionNueva -> capacidad));
+
+    printf("Ingrese (en minutos) cuanto tiempo dura el recorrido de la atracción\n");
+    scanf("%d", &(atraccionNueva -> duracion));
+
+    printf("Ingrese (en metros, formato x.xx) la altura minima para subir a la atracción\n");
+    scanf("%f", &(atraccionNueva -> alturaMinima));
+
+    printf("Ingrese la edad minima para subir a la atracción\n");
+    scanf("%d", &(atraccionNueva -> edadMinima));
+
+    /* valores default de la atracción */
+    atraccionNueva -> visitantesTotales = 0;
+    atraccionNueva -> estado = 2; /* fuera de servicio*/
+    atraccionNueva -> headFila = NULL;
+
+    nuevoNodo -> datos = atraccionNueva;
+    nuevoNodo -> sig = NULL;
+    nuevoNodo -> ant = NULL;
+
+    return nuevoNodo;
+}
+
+void agregarAtraccion(struct Zona **zonas, int *pLibreZonas){
+    struct NodoAtraccion *atraccionNueva = NULL;
+    struct NodoAtraccion *rec;
+    struct Zona *zonaElegida;
+    int i, zona;
+
+    atraccionNueva = crearAtraccion();
+
+    /*Selección de zona*/
+    printf("==============================================\n");
+    printf("Lista de Zonas\n");
+    printf("==============================================\n");
+
+    for(i = 0; i < *pLibreZonas; i++){
+        printf("%d: %s\n", i, zonas[i] -> nombre);
+    }
+    printf("\n");
+
+    scanf("Ingrese el numero de la zona a la que desea agregar la nueva atracción %d\n", &zona);
+    if(zona > *pLibreZonas){
+        do{
+            scanf("Ingrese un numero válido %d\n", &zona);
+        }while(zona > *pLibreZonas);
+    }
+
+    /*Agregado de la atraccion nueva a la lista de atracciones*/
+    zonaElegida = zonas[i];
+    rec = zonaElegida -> headAtracciones -> sig;
+
+    while(rec -> sig != NULL){
+        rec = rec -> sig;
+    }
+
+    rec -> sig = atraccionNueva;
+    rec -> sig -> ant = rec;
+    rec -> sig -> sig = NULL;
+}
+
+void cerrarAtraccion(struct NodoAtraccion *atraccionACerrar, int razon){
+
+    if(razon != 3){
+        printf("Ingrese la razon de cierre\n");
+        printf("1 = En Mantenimiento\n");
+        printf("2 = Fuera de servicio\n");
+        scanf("%d", &razon);
+    }
+
+    /* cambia el estado y vacia la fila de espera */
+    atraccionACerrar -> datos -> estado = razon;
+    atraccionACerrar -> datos -> headFila -> sig = atraccionACerrar -> datos -> headFila;
+}
+
 int main(){
-    printf("Hola profe ponganos un 7");
+    printf("Profe ponganos un 7 xd\n");
     return 0;
 }
