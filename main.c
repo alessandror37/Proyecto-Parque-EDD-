b02b5d269e8169b7fa8d60c463cc78281fe725d7
SilentlyJon <j.gutierrez1764@gmail.com>
2026-05-28 01:50:44 -0400

    Se agregaron funciones para gestion de zonas y atracciones:
    -recomendarZonas, funcion que deberá ser llamada por la funcion que actualiza el contador ocupacionActual cada vez que una persona entra en una zona y esa zona tenga una ocupacion por sobre el 90% de su capacidad, como parte de las comprobaciones y actualizaciones que se hacen en ese momento.
    
    -ordenarZonasPorTiempoDeEspera, funcion auxiliar del qsort usado en recomendarZonas.
    
    -calcularTiempoEsperaMedio, funcion auxiliar de la funcion ordenarZonasPorTiempoDeEspera, calcula el tiempo de espera medio de una zona.
    
    -verificarYAsignarPersonal, funcion que verifica el estado de ocupacion de una zona y le asigna personal en caso de necesitarlo, o devuelve el personal a la cantidad predeterminada si ya no lo necesita, pasandolo a la zona con menos personal en ese momento. Esta funcion debera ser llamada también cada vez que una persona entre a una zona y se actualice su contador de ocupacionActual
    
    -buscarZonaConMenosPersonal, funcion auxiliar de verificarYAsignarPersonal, retorna la zona con menos personal, puede ser reutilizable en otros contextos.
    
    -buscarZonaConMenosVisitantes, funcion auxiliar de verificarYAsignarPersonal, retorna la zona con menos visitantes en ese momento, tambien reutilizable en otro contexto.
    
    -menuVisitantesEnZona, menú provisional de las opciones relacionadas con los visitantes, no es muy relevante, se puede eliminar luego.
    
    -consultarZonaConMasVisitantes, funcion que sirve como una opcion de un menú, no retorna nada, imprime la zona con mas visitantes en ese momento.
    
    -consultarCantidadVisitantesPorZona, funcion que tambien es una opcion de un menú, no retorna nada, pide al usuario elegir una zona e imprime la cantidad de visitantes en esa zona.
    
    -seleccionDeZona, funcion universal que sirve para elegir una zona, le muestra al usuario la lista de zonas, y le pide seleccionar una, retorna el indice de esa zona en el arreglo de struct Zona **zonas.

--- main.c
+++ main.c
@@ -65,6 +65,8 @@ struct Zona{
     char *horaCierre;
     int capacidad;
     int personalEncargado;
+    int ocupacionActual;
+    int ocupacionHistorica;
     struct NodoAtraccion *headAtracciones; /*head a la lista de atracciones*/
 };
 
@@ -86,6 +88,31 @@ char *pasarAMinus(char *cadena){
     }
     return NuevaCadena;
 }
+
+int seleccionDeZona(struct Zona **zonas, int pLibreZonas){
+    int zona, i;
+
+    printf("==============================================\n");
+    printf("Lista de Zonas\n");
+    printf("==============================================\n");
+
+    for(i = 0; i < pLibreZonas; i++){
+        printf("%d: %s\n", i, zonas[i] -> nombre);
+    }
+    printf("\n");
+
+    printf("Ingrese el numero de la zona a utilizar\n");
+    scanf("%d", &zona);
+    if(zona > pLibreZonas){
+        do{
+            printf("Ingrese un numero válido\n");
+            scanf("%d", &zona);
+        }while(zona > pLibreZonas);
+    }
+
+    return zona;
+}
+
 
 struct NodoAtraccion *crearAtraccion(){
     /*Asignacion de memoria a los nuevos struct e inicialización de variables*/
@@ -129,29 +156,16 @@ void agregarAtraccion(struct Zona **zonas, int *pLibreZonas){
     struct NodoAtraccion *atraccionNueva = NULL;
     struct NodoAtraccion *rec;
     struct Zona *zonaElegida;
-    int i, zona;
+    int zona;
 
     atraccionNueva = crearAtraccion();
 
     /*Selección de zona*/
-    printf("==============================================\n");
-    printf("Lista de Zonas\n");
-    printf("==============================================\n");
-
-    for(i = 0; i < *pLibreZonas; i++){
-        printf("%d: %s\n", i, zonas[i] -> nombre);
-    }
-    printf("\n");
-
-    scanf("Ingrese el numero de la zona a la que desea agregar la nueva atracción %d\n", &zona);
-    if(zona > *pLibreZonas){
-        do{
-            scanf("Ingrese un numero válido %d\n", &zona);
-        }while(zona > *pLibreZonas);
-    }
+    printf("¿A que zona se agregará la nueva atracción?\n");
+    zona = seleccionDeZona(zonas, *pLibreZonas);
 
     /*Agregado de la atraccion nueva a la lista de atracciones*/
-    zonaElegida = zonas[i];
+    zonaElegida = zonas[zona];
     rec = zonaElegida -> headAtracciones -> sig;
 
     while(rec -> sig != NULL){
@@ -164,7 +178,7 @@ void agregarAtraccion(struct Zona **zonas, int *pLibreZonas){
 }
 
 void cerrarAtraccion(struct NodoAtraccion *atraccionACerrar, int razon){
-
+    /* Si la razon de cierre es fuera de horario simplemente se cierra */
     if(razon != 3){
         printf("Ingrese la razon de cierre\n");
         printf("1 = En Mantenimiento\n");
@@ -176,6 +190,182 @@ void cerrarAtraccion(struct NodoAtraccion *atraccionACerrar, int razon){
     atraccionACerrar -> datos -> estado = razon;
     atraccionACerrar -> datos -> headFila -> sig = atraccionACerrar -> datos -> headFila;
 }
+
+
+
+void consultarCantidadVisitantesPorZona(struct Zona **zonas, int pLibreZonas){
+    int zona;
+    struct Zona *zonaElegida;
+
+    /* fase seleccion de zona */
+    printf("CANTIDAD DE VISITANTES POR ZONA\n\n");
+    zona = seleccionDeZona(zonas, pLibreZonas);
+
+    /* recorrido de arbol */
+    zonaElegida = zonas[zona];
+
+    printf("La cantidad de personas en la zona %s es de: %d \n", zonaElegida -> nombre, zonaElegida -> ocupacionActual);
+}
+
+void consultarZonaConMasVisitantes(struct Zona **zonas, int pLibreZonas){
+    int i, maxVisitantes, actual;
+    struct Zona *zonaElegida;
+
+    zonaElegida = zonas[0];
+    maxVisitantes = zonas[0] -> ocupacionActual;
+
+    for(i = 1; i < pLibreZonas; i++){
+        actual = zonas[i] -> ocupacionActual;
+        if(actual > maxVisitantes){
+            zonaElegida = zonas[i];
+            maxVisitantes = actual;
+        }
+    }
+
+    printf("La zona con mas personas es: %s con: %d personas actualmente\n", zonaElegida -> nombre, maxVisitantes);
+}
+
+void menuVisitantesEnZona(struct Zona **zonas, int pLibreZonas){
+    int opcion;
+
+    /* Menu de Visitantes */
+    printf("==============================================\n");
+    printf("MENU DE VISITANTES\n");
+    printf("==============================================\n \n");
+    printf("OPCIONES\n");
+    printf("1. Consultar cantidad de visitantes por zona\n");
+    printf("2. Consultar zona con mas visitantes\n");
+    scanf("%d", &opcion);
+
+    if(opcion < 1 || opcion > 2){
+        do{
+            printf("Ingrese un numero válido\n");
+            scanf("%d", &opcion);
+        }while(opcion < 1 || opcion > 2);
+    }
+
+    if(opcion == 1){
+        consultarCantidadVisitantesPorZona(zonas, pLibreZonas);
+    }else if(opcion == 2){
+        consultarZonaConMasVisitantes(zonas, pLibreZonas);
+    }
+}
+
+struct Zona *buscarZonaConMenosVisitantes(struct Zona **zonas, int pLibreZonas){
+    struct Zona *zonaElegida;
+    int menorCantVisitantes, i;
+
+    zonaElegida = zonas[0];
+    menorCantVisitantes = zonas[0] -> ocupacionActual;
+
+    for(i = 1; i < pLibreZonas; i++) {
+        if(zonas[i] -> ocupacionActual < menorCantVisitantes) {
+            zonaElegida = zonas[i];
+            menorCantVisitantes = zonas[i] -> ocupacionActual;
+        }
+    }
+    return zonaElegida;
+}
+
+struct Zona *buscarZonaConMenosPersonal(struct Zona **zonas, int pLibreZonas){
+    struct Zona *zonaElegida;
+    int menorCantPersonal, i;
+
+    zonaElegida = zonas[0];
+    menorCantPersonal = zonas[0] -> personalEncargado;
+
+    for(i = 1; i < pLibreZonas; i++) {
+        if(zonas[i] -> personalEncargado < menorCantPersonal) {
+            zonaElegida = zonas[i];
+            menorCantPersonal = zonas[i] -> personalEncargado;
+        }
+    }
+    return zonaElegida;
+}
+
+void verificarYAsignarPersonal(struct Zona **zonas, int pLibreZonas, int idZona){
+    int i, capacidad, ocupacion;
+    struct Zona *zonaConMenosVisitantes,*zonaConMenosPersonal, *zonaARevisar;
+
+    for(i = 0; i < pLibreZonas; i++){
+        if(zonas[i] -> codigo == idZona){
+            zonaARevisar = zonas[i];
+        }
+    }
+
+    capacidad = zonaARevisar -> capacidad;
+    ocupacion = zonaARevisar -> ocupacionActual;
+
+    /* si la zona se encuentra a mas de un 90% de su ocupacion y su personal no se ha aumentado nunca, se trae 5 personas de la zona con menos visitantes en ese momento*/
+    if(ocupacion > capacidad * 0.9){
+        if(zonaARevisar -> personalEncargado == 30){
+            printf("LA ZONA ACTUAL TIENE UNA OCUPACION ALTA, SE ENVIARÁ PERSONAL DESDE OTRAS ZONAS\n");
+            zonaARevisar -> personalEncargado += 5;
+            zonaConMenosVisitantes = buscarZonaConMenosVisitantes(zonas, pLibreZonas);
+            zonaConMenosVisitantes -> personalEncargado -= 5;
+        }
+    }else{
+        /* si la zona no está sobre el 90% de ocupacion pero tiene personal de mas, este personal se mueve a la zona con menos personal en ese momento */
+        if(zonaARevisar -> personalEncargado > 30){
+            printf("LA ZONA ACTUAL TIENE UN EXCESO DE PERSONAL, SE ENVIARA PARTE DE SU PERSONAL A OTRA ZONA");
+            zonaARevisar -> personalEncargado -= 5;
+            zonaConMenosPersonal = buscarZonaConMenosPersonal(zonas, pLibreZonas);
+            zonaConMenosPersonal -> personalEncargado += 5;
+        }
+    }
+}
+
+
+
+int calcularTiempoEsperaMedio(const struct Zona *zona){
+    /* esta es la funcion auxiliar que usa la funcion del qsort para comparar */
+    struct NodoAtraccion *rec;
+    int contadorAtracciones = 0;
+    int acumuladorTiempos = 0;
+    rec = zona -> headAtracciones;
+
+    while(rec -> sig != NULL) {
+        acumuladorTiempos += tiempoEsperaEstimado(rec -> datos);
+        contadorAtracciones ++;
+        rec = rec -> sig;
+    }
+
+    return acumuladorTiempos/contadorAtracciones;
+}
+
+int ordenarZonasPorTiempoDeEspera(const void *a, const void *b){
+    const struct Zona *zonaA = *(const struct Zona **)a;
+    const struct Zona *zonaB = *(const struct Zona **)b;
+
+    int tiempoA = calcularTiempoEsperaMedio(zonaA);
+    int tiempoB = calcularTiempoEsperaMedio(zonaB);
+
+    if (tiempoA < tiempoB) return -1;
+    if (tiempoA > tiempoB) return 1;
+    return 0;
+}
+
+void recomendarZonas(struct Zona **zonas, int pLibreZonas){
+    struct Zona **zonasEnOrden = (struct Zona **)malloc(pLibreZonas * sizeof(struct Zona *));
+    int i, limite;
+
+    memcpy(zonasEnOrden, zonas, pLibreZonas * sizeof(struct Zona *));
+    qsort(zonasEnOrden, (size_t)pLibreZonas, sizeof(struct Zona *), ordenarZonasPorTiempoDeEspera);
+
+    printf("LA ZONA ACTUAL TIENE UN TIEMPO DE ESPERA MUY ALTO\n");
+    printf("recomendación de zonas con menos tiempos de espera:\n");
+
+    if(pLibreZonas >= 3) {
+        limite = 3;
+    }else{
+        limite = pLibreZonas - 1;
+    }
+
+    for(i = 0; i < limite; i++){
+        printf("%s \n", zonasEnOrden[i] -> nombre);
+    }
+}
+
 
 int main(){
     printf("Profe ponganos un 7 xd\n");
